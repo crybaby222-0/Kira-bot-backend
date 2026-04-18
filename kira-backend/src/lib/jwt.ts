7a6441f782f6137147d0b7148ddfea9c4fa9ba9a
littlebodytobigheat54 <josefornewphone@gmail.com>
2026-04-18 13:47:14 -0300

    fix real errors

--- kira-backend/src/lib/jwt.ts
+++ kira-backend/src/lib/jwt.ts
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export function signToken(payload: any) {
-  return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: "7d",
-  });
+  return jwt.sign(
+    payload,
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "7d",
+    }
+  );
 }
